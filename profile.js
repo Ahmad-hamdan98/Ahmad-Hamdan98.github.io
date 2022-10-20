5711f8466d2af45fa561007bcad0ec8244edcd46
ahmad-hamdan98 <ahmadhamdan8592@gmail.com>
2022-10-20 16:55:38 +0300

    update

---
+++ profile.js
@@ -0,0 +1,19 @@
+function reb(Element){
+    document.getElementById("replace").innerText="D.Amin ";
+}
+
+
+counter=2;
+function rem1(Element){
+    document.getElementsById("www").remove();
+    counter+1;
+    document.getElementsByClassName("qa1").innerText=counter;
+
+}
+function rem2(){
+    document.getElementsById("remove1").remove();
+    counter++;
+    document.getElementsByClassName("qa1").innerText=counter;
+
+}
+
